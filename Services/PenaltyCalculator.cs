110bb3f886c7f70dce5573e95961576b74ebf9a9
Arsentii Turlo <s34233@pjwstk.edu.pl>
2026-03-24 00:23:57 +0100

    Created and implemented PenaltyCalculator Class

---
+++ Services/PenaltyCalculator.cs
@@ -0,0 +1,17 @@
+using apbd_cw2_oop_s34233.Domain;
+using Microsoft.VisualBasic;
+
+namespace apbd_cw2_oop_s34233.Services;
+
+public static class PenaltyCalculator
+{
+    private const decimal PenaltyPerDay = 5m;
+
+    public static decimal CalculatePenalty(DateTime dueDate, DateTime returnDate)
+    {
+        if (returnDate <= dueDate)
+            return 0m;
+        int daysLate = (int)(returnDate - dueDate).TotalDays;
+        return daysLate * PenaltyPerDay;
+    }
+}
