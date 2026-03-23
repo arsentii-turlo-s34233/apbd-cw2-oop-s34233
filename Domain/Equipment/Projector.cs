dc2edc450e24e2cf7fe797dd3cbc3be44cb00cb3
Arsentii Turlo <s34233@pjwstk.edu.pl>
2026-03-23 22:41:24 +0100

    Created and implemented Projector Class

---
+++ Domain/Equipment/Projector.cs
@@ -0,0 +1,18 @@
+namespace apbd_cw2_oop_s34233.Domain.Equipment;
+
+public class Projector : Equipment
+{
+    public int LumensOutput { get; set; }
+    public string Resolution { get; set; }
+
+    public Projector(string name, int lumens,  string resolution) : base(name)
+        {
+        LumensOutput = lumens;
+        Resolution = resolution;
+        }
+
+    public override string GetDescription()
+    {
+        return $"Projector: {Name} | Lumens: {LumensOutput} lm | Resolution: {Resolution}";
+    }
+}
